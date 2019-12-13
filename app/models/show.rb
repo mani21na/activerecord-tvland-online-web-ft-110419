2b38cdcf652148191040abd64f4ac90974478083
Hanna Lee <mani21na@gmail.com>
2019-12-13 23:25:06 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,11 +4,6 @@ class Show < ActiveRecord::Base
   belongs_to :network
   
   def actors_list
-    self.characters.collect do |c|
-      c.actors.name
-    end 
-    
-  def actors_list
     self.actors.map{|actor| actor.full_name}
   end
   end 
