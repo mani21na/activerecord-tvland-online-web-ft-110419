9bff2c3ead8e3a1b8e6c9f52ee9fe8f1f7d624c8
Hanna Lee <mani21na@gmail.com>
2019-12-13 22:57:40 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,8 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    characters.collect do |character|
-      "#{character.name} - #{character.show.name}"
+    characters.collect do |c|
+      "#{c.name} - #{c.show.name}"
     end
   end
 end
